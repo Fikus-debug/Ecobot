6ba9fdc2972af4c343e813f02897625af101aa31
Fikus-debug <iri584879@mail.ru>
2026-02-23 16:40:57 +0300

    Update ecohelper.py
    Removed my bot token

--- ecohelper.py
+++ ecohelper.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("8551596351:AAG1-TQf2Tz3cg7ijWS6JYbMPkbSskXluQk")
+bot = telebot.TeleBot("")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -29,5 +29,6 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
         bot.reply_to(message, message.text)
+
 
 bot.polling()
